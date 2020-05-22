93340e5b85b09eaaabf23e7761d7c3eb243b6616
aaronmccormack15533 <maron.accormack@gmail.com>
2020-05-22 12:47:29 +0100

    Appending to test sheet

--- index.js
+++ index.js
@@ -19,15 +19,38 @@ client.authorize(function(err, tokens){
 });
 
 async function gsrun(cl){
-    const gsapi = google.sheets({version:'v4', auth: cl });
+    const gsapi = google.sheets({version:'v4'});
 
-    const opt = {
-        spreadsheetId: '18NmM9GqkKHkeysfgG3_ljjypDbdwuFQG7gec4lxWhGQ',
-        range: '2020 Logs'
-    };
+    // const opt = {
+    //     spreadsheetId: '18NmM9GqkKHkeysfgG3_ljjypDbdwuFQG7gec4lxWhGQ',
+    //     range: '2020 Logs'
+    // };
 
-    let data = await gsapi.spreadsheets.values.get(opt);
-    console.log(data.data.values);
+    // let fullSheet = await gsapi.spreadsheets.values.get(opt);
+    // console.log(fullSheet.data.values);
+
+    const request = {
+        // spreadsheetId: '18NmM9GqkKHkeysfgG3_ljjypDbdwuFQG7gec4lxWhGQ',
+        // range: '2020 Logs',
+        spreadsheetId: '1ByqGxCjhGTu7nvQq29FpMI-G3VPajSD6OjWydIgEIdI',
+        range: 'Sheet1',
+        valueInputOption: 'RAW',
+        insertDataOption: 'INSERT_ROWS',
+
+        resource: {
+            values: [
+                ["testappend", "testappend2"]
+            ]
+        },
+        auth: cl
+    }
+
+    try{
+        const response = (await gsapi.spreadsheets.values.append(request)).data;
+        console.log(JSON.stringify(response, null, 2));
+    } catch(err){
+        console.log(err);
+    }
     
 }
 
